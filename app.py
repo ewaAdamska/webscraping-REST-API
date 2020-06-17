540d3361d62532169d19a1abdab35c394326a8fe
ewaAdamska <adamska.ew@gmail.com>
2020-06-17 18:11:22 +0200

    api's secret key hidden in environment variable

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ from db import db
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.secret_key = os.environ.get('ABI_SECRET_KEY')
+app.secret_key = os.environ.get('API_SECRET_KEY')
 api = Api(app)
 
 @app.before_first_request
